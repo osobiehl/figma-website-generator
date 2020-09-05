b3c31caa9c759e97faefaadf7384e3d40f789b4c
BuildTools <unconfigured@null.spigotmc.org>
2020-09-05 17:18:45 +0200

    added a percentage creation mode for websites

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const https = require('https');
 fs = require('fs');
 
 var req = new XMLHttpRequest();
-const HTMLHEAD = `<!DOCTYPE html>
+var HTMLHEAD = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="index.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Muli:500,600,700,800,900&display=swap" rel="stylesheet">
 </head>
 <body>`
+var CSSHEAD;
 
 
 const readline = require('readline').createInterface({
@@ -28,8 +31,12 @@ var modifier = Config.mod
 var imageStringArray = [];
 const outputHTML = "index.html";
 const outputCSS = "index.css";
-var imageMap;
-var rawImageMap;
+var min_x_position, min_y_position;
+var max_x_position, max_y_position;
+//used for percentage calculations
+var max_width;
+//also if using heights as a percentage is enabled
+var max_height;
 
 
 
@@ -46,6 +53,17 @@ req.onload=function(){
     initializeRender(incomingJson);
 }
 function AddToImageString(node){
+    if (node.absoluteBoundingBox !== undefined && Config.FindLargestCoords === true){
+        let bb = node.absoluteBoundingBox;
+        if (bb.x > max_x_position)
+            max_x_position = bb.x;
+        if (bb.x < min_x_position)
+            min_x_position = bb.x;
+        if (bb.y > max_y_position)
+            max_y_position = bb.y;
+        if (bb.y < min_y_position)
+            min_y_position = bb.y;
+    }
     if(node.fills === undefined)
         return;
     else if (node.fills[0] === undefined)
@@ -61,10 +79,19 @@ function stderr(err){
 }
 
 function initializeRender(incomingJson){
+
     fs.writeFileSync(outputHTML, HTMLHEAD, stderr);
 
     let JSONRoot = incomingJson.nodes;
-    for (var id in JSONRoot){
+    for (var  id in JSONRoot){
+        max_x_position = -1999999;
+        max_y_position = -1999999;
+        min_y_position = 1999999;
+        min_x_position = 1999999;
+
+        min_y_position = JSONRoot[id].document.absoluteBoundingBox.y;
+        min_x_position = JSONRoot[id].document.absoluteBoundingBox.x;
+
         treeIteration(JSONRoot[id].document, 0, AddToImageString);
         imageStringArray[0] = imageStringArray[0].substring(1);
         // calls GET for all requested images
@@ -77,10 +104,24 @@ function initializeRender(incomingJson){
         let test = JSON.parse(img_req.responseText);
         imageMap = (JSON.parse(img_req.responseText));
         console.log(imageMap);
-
-        //now creates static html page
-        treeIteration(JSONRoot[id].document, 0, WriteOutput);
-
+        if (Config.UseWidthPercentage === true) {
+            max_width = JSONRoot[id].document.absoluteBoundingBox.width;
+             max_height = JSONRoot[id].document.absoluteBoundingBox.height;
+            fs.appendFileSync(outputHTML, `<div class="content-wrapper">`, stderr);
+            CSSHEAD = `.content-wrapper{
+max-width: ${max_width}px;
+height: ${max_height}px;
+overflow: hidden;
+position: relative;
+margin-left: auto;
+}`
+            fs.writeFileSync(outputCSS, CSSHEAD, stderr);
+            treeIteration(JSONRoot[id].document, 0, WriteOutputPercentage);
+        }
+        else {
+            //now creates static html page
+            treeIteration(JSONRoot[id].document, 0, WriteOutput);
+        }
     }
 
 
@@ -135,8 +176,8 @@ ${node.characters}
 position: absolute;`
             //case: bounding box is defined
             if (node.absoluteBoundingBox !== undefined) {
-                CSSText += `top: ${1 * node.absoluteBoundingBox.y + 3000 + 1443}px;
-left: ${1 * node.absoluteBoundingBox.x + 4332.7460937}px;
+                CSSText += `top: ${1 * node.absoluteBoundingBox.y -min_y_position}px;
+left: ${1 * node.absoluteBoundingBox.x -min_x_position}px;
 width: ${node.absoluteBoundingBox.width}px;
 height: ${node.absoluteBoundingBox.height}px;\n`;
             }
@@ -168,19 +209,115 @@ height: ${node.absoluteBoundingBox.height}px;\n`;
             CSSText += `${node.type === "TEXT"? 'color: ': 'background-color: '} rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${opacity === undefined? color.a : opacity});\n`
 
         }
+        //case: border is defined (currently only solid border);
         if (node.strokes !== undefined && node.strokes.length > 0){
             let strokes = node.strokes[0]
             CSSText += `border: ${node.strokeWeight}px solid rgba(${strokes.color.r * 255}, ${strokes.color.g * 255}, ${strokes.color.b * 255}, ${strokes.color.a});\n`;
         }
+        //case: corner radius is defined;
         if (node.cornerRadius !== undefined ){
             CSSText +=  `border-radius: ${node.cornerRadius}px;\n`
         }
         CSSText +='\n}';
         fs.appendFileSync(outputHTML, HtmlText, stderr);
         fs.appendFileSync(outputCSS, CSSText, stderr);
+    }
 }
 
+function WriteOutputPercentage(node) {
+    let HtmlText;
+    let CSSText;
+    var temp = node.id;
+    temp = temp.replace(/:/g, '-');
+    temp = temp.replace(/;/g, '-');
+    const parsedId = 'css-' + temp;
+    const wtf = node.id;
+    console.log("node is: " + node.id);
+    let acceptedCase = false;
+    if (node.fills !== undefined && node.fills[0] !== undefined && node.fills[0].type === "IMAGE") {
 
+        HtmlText = `<div class="${parsedId}">
+<img src="${imageMap.images[wtf]}" alt="" class="${parsedId}-img"> 
+</div>`;
+        acceptedCase = true;
+    }
+    else if (node.type !== undefined && node.type === "TEXT") {
+        HtmlText = `<div class = "${parsedId}">
+${node.characters}
+</div>`;
+        acceptedCase = true;
+    }
+    else if (node.type === "GROUP" || node.type === "RECTANGLE" || node.type === "LINE"){
+        let color = null;
+        if (node.fills && node.fills[0])
+            color = node.fills[0].color;
+        HtmlText = `<div class=${parsedId}></div>`
+        acceptedCase = true;
+    }
+    //CSS constructor case "business bus"
+    if (acceptedCase === true){
+        let ImageCSS = '';
+        CSSText = `.${parsedId}{
+position: absolute;`
+        //case: bounding box is defined
+        if (node.absoluteBoundingBox !== undefined) {
+            CSSText += `top: ${1 * node.absoluteBoundingBox.y -min_y_position}px;
+left: ${(1 * node.absoluteBoundingBox.x -min_x_position) / max_width * 100}%;
+width: ${node.absoluteBoundingBox.width /max_width * 100}%;
+height: ${node.absoluteBoundingBox.height}px;\n`;
+        }
+        //case: style is defined
+        if (node.style !== undefined){
+            CSSText +=`font-weight: ${node.style.fontWeight};
+        font-size: min(${node.style.fontSize}px, ${node.style.fontSize / max_width * 200}vh);
+        line-height: ${node.style.lineHeightPx}px ;
+        font-family: ${node.style.fontFamily === undefined ? "inherit" : node.style.fontFamily};\n`;
+            switch (node.style.textAlignHorizontal){
+                case("CENTER"):
+                    CSSText += 'text-align: center;'
+                    break;
+                case("LEFT"):
+                    CSSText += 'text-align: left;'
+                    break
+                case(undefined):
+                    break;
+                default:
+                    break;
+            }
 
+        }
+        //case: defining the color / background color and opacity
+
+        if (node.fills !== undefined && node.fills.length > 0){
+            if (node.fills[0].color !== undefined) {
+                let color = node.fills[0].color;
+                let opacity = node.fills[0].opacity;
+                CSSText += `${node.type === "TEXT" ? 'color: ' : 'background-color: '} rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${opacity === undefined ? color.a : opacity});\n`
+            }
+            if (node.fills[0].type === "IMAGE"){
+                ImageCSS = `.${parsedId} img{
+width: 100%;
+height: 100%;                               
+}`
+            }
+
+        }
+        //case: border is defined (currently only solid border);
+        if (node.strokes !== undefined && node.strokes.length > 0){
+            let strokes = node.strokes[0]
+            CSSText += `border: ${node.strokeWeight}px solid rgba(${strokes.color.r * 255}, ${strokes.color.g * 255}, ${strokes.color.b * 255}, ${strokes.color.a});\n`;
+        }
+        //case: corner radius is defined;
+        if (node.cornerRadius !== undefined ){
+            CSSText +=  `border-radius: ${node.cornerRadius}px;\n`
+        }
+
+        //case: our group should have a fixed size
+        CSSText +='}\n';
+        CSSText += ImageCSS;
+        fs.appendFileSync(outputHTML, HtmlText, stderr);
+        fs.appendFileSync(outputCSS, CSSText, stderr);
+    }
 }
+
 
